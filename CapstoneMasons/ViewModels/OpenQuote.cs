4c9eab6bc2a0271184164421e4e7378f2ad1f447
DaneWoods <49732735+DaneWoods@users.noreply.github.com>
2020-04-20 16:27:16 -0700

    Figuring out how to write the view

---
+++ CapstoneMasons/ViewModels/OpenQuote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CapstoneMasons.Models;
+
+namespace CapstoneMasons.ViewModels
+{
+    public class OpenQuote
+    {
+        private List<Shape> shapes = new List<Shape>();
+        private List<Cost> costs = new List<Cost>();
+
+        public int QuoteID { get; set; }
+        public string Name { get; set; } //After Review
+        public string OrderNum { get; set; } //After Review
+        public List<Shape> Shapes //Before Review
+        {
+            get { return shapes; }
+        }
+        public decimal TotalCost { get; set; }
+        public List<Cost> Costs //Before Review
+        {
+            get { return costs; }
+        }
+        public DateTime DateQuoted { get; set; } //Before Review
+        public bool PickedUp { get; set; } //After Review
+        public bool Open { get; set; } //After Review
+        public bool? AddSetup { get; set; } //After Review
+        public decimal Discount { get; set; } //After Review
+    }
+}
