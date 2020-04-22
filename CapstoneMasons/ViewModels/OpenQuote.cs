bc948e8c557268d86c5457db057d0821ae251c22
DaneWoods <49732735+DaneWoods@users.noreply.github.com>
2020-04-22 10:53:58 -0700

    Progress bars now working

--- CapstoneMasons/ViewModels/OpenQuote.cs
+++ CapstoneMasons/ViewModels/OpenQuote.cs
@@ -8,25 +8,8 @@ namespace CapstoneMasons.ViewModels
 {
     public class OpenQuote
     {
-        private List<Shape> shapes = new List<Shape>();
-        private List<Cost> costs = new List<Cost>();
-
-        public int QuoteID { get; set; }
-        public string Name { get; set; } //After Review
-        public string OrderNum { get; set; } //After Review
-        public List<Shape> Shapes //Before Review
-        {
-            get { return shapes; }
-        }
+        private List<Quote> quotes = new List<Quote>();
+        public List<Quote> Quotes { get { return quotes; } }
         public decimal TotalCost { get; set; }
-        public List<Cost> Costs //Before Review
-        {
-            get { return costs; }
-        }
-        public DateTime DateQuoted { get; set; } //Before Review
-        public bool PickedUp { get; set; } //After Review
-        public bool Open { get; set; } //After Review
-        public bool? AddSetup { get; set; } //After Review
-        public decimal Discount { get; set; } //After Review
     }
 }
