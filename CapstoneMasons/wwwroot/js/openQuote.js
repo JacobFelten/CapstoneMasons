650429b27f8df3150681d536459773c88f86e858
betettag <betettag@my.lanecc.edu>
2020-05-09 13:24:54 -0700

    edditing a shape in an open quote peek at the changes if you want (there are many) framework done preselecting an angle will be hard so i left it uncheked deleting a leg will be similar to deleting a shape once i fix the delete shape bugs

---
+++ CapstoneMasons/wwwroot/js/openQuote.js
@@ -0,0 +1,5 @@
+﻿function getShapeNames(id) {
+    var shapeName = document.getElementById(`Shape${id}.Title`).innerHTML;
+    var shapeModal = document.getElementById(`Shape${id}.Modal`).innerHTML = "Editing " + shapeName;
+    //console.log(shapeModal); testing
+}
