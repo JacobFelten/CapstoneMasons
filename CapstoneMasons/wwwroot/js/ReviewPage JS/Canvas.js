ea009b829623bce385c4996c349fd9f585c0f490
jeffw773 <49732785+jeffw773@users.noreply.github.com>
2020-05-16 20:46:30 -0700

    Added the ability to draw the Mandrel dimensions dynamically when drawing the shapes. Also reworked the label centering code so that the labels end up in between the mandrel bend areas

--- CapstoneMasons/wwwroot/js/ReviewPage JS/Canvas.js
+++ CapstoneMasons/wwwroot/js/ReviewPage JS/Canvas.js
@@ -6,6 +6,7 @@ var canvas = document.getElementById(canvasID);
 if (canvas.getContext) {
 
     //global variables for canvas
+    var BarSize = legs[0].Size; //sets the barsize from the legs being passed in
     var scalar = 1;
     var min_x;
     var max_x;
@@ -38,6 +39,22 @@ if (canvas.getContext) {
     //initialize x and y arrays
     xcoords = {};
     ycoords = {};
+
+
+    //variables for drawing mandrel radii onto shapes
+    //initialize OSSB point arrays
+    ossbX1 = {};
+    ossbY1 = {};
+    ossbX2 = {};
+    ossbY2 = {};
+    OSSB_length = {};
+
+    //initialize the Apex point arrays
+    apexX = {};
+    apexY = {};
+
+
+
 
     //Keep track of the unit circle positions for potential Leg Labeling
     unit_val_list = {};
@@ -240,6 +257,7 @@ if (canvas.getContext) {
         }
     }//End of Legs in a single shape loop
 
+    console.log("Current Shape is on " + canvasID);
     console.log("Middle of tiny shape currently lies on (" + (max_x + min_x) / 2 + "," + (max_y + min_y) / 2 + ")");
 
 
@@ -396,6 +414,84 @@ if (canvas.getContext) {
         }
     }
 
+    //Now that shape has been scaled up find out where the APEX and OSSB points are
+    curr_unit_val = 0
+
+    //Save the Apex point values
+    for (var coord = 0; coord < Object.keys(xcoords).length - 2; coord++) {
+        apexX[coord] = xcoords[coord + 1];
+        apexY[coord] = ycoords[coord + 1];
+    }
+
+    for (var j = 0; j < legs.length; j++) {
+
+        //as long as its not the last leg of the shape, save these temp variables
+        if (j != legs.length - 1)
+        { 
+            var Thickness_mm = (BarSize / 8) * 25.4;
+            var Radius_mm = legs[j].Mandrel * 25.4;
+            var comp_angle = 180 - legs[j].Degree;
+            var radian_angle = comp_angle * Math.PI / 180;
+            var OSSB = Math.tan(radian_angle / 2) * (Thickness_mm + Radius_mm);
+            OSSB = OSSB / 25.4;         //convert OSSB back into inches from millimeters
+            OSSB_length[j] = OSSB;
+            
+        }
+        //if leg is the first leg
+        if (j == 0)
+        {
+            ossbX1[j] = xcoords[0] + legs[0].Length * scalar - OSSB_length[j] * scalar;
+            //save the OSSB point where the arcTo first begins
+            ossbY1[j] = ycoords[0];
+            //save the OSSB point where the arcTo first begins
+        }
+        //if the leg is the last leg
+        else if (j == legs.length - 1)
+        {
+            if (legs[j - 1].IsRight == true) {
+                neg_pos = 1;
+            }
+            else {
+                neg_pos = -1;
+            }
+
+            //save the previous OSSB point
+            ossbX2[j - 1] = xcoords[j] + scalar * Math.cos(((Math.PI) / 180) * (curr_unit_val + (neg_pos) * (180) - (neg_pos) * (legs[j - 1].Degree))) * OSSB_length[j - 1];
+            ossbY2[j - 1] = ycoords[j] + scalar * Math.sin(((Math.PI) / 180) * (curr_unit_val + (neg_pos) * (180) - (neg_pos) * (legs[j - 1].Degree))) * OSSB_length[j - 1];
+
+            curr_unit_val = curr_unit_val + (neg_pos) * (180) - (neg_pos) * (legs[j - 1].Degree);
+        }
+        //if leg is any leg between the first and last leg
+        else
+        {
+            if (legs[j - 1].IsRight == true)
+            {
+                neg_pos = 1;
+            }
+            else
+            {
+                neg_pos = -1;
+            }
+
+            //save the previous OSSB point
+            ossbX2[j - 1] = xcoords[j] + scalar * Math.cos(((Math.PI) / 180) * (curr_unit_val + (neg_pos) * (180) - (neg_pos) * (legs[j - 1].Degree))) * OSSB_length[j - 1];
+            ossbY2[j - 1] = ycoords[j] + scalar * Math.sin(((Math.PI) / 180) * (curr_unit_val + (neg_pos) * (180) - (neg_pos) * (legs[j - 1].Degree))) * OSSB_length[j - 1];
+
+            //save the next OSSB point
+            ossbX1[j] = xcoords[j] + scalar * Math.cos(((Math.PI) / 180) * (curr_unit_val + (neg_pos) * (180) - (neg_pos) * (legs[j - 1].Degree))) * legs[j].Length - scalar * Math.cos(((Math.PI) / 180) * (curr_unit_val + (neg_pos) * (180) - (neg_pos) * (legs[j - 1].Degree))) * OSSB_length[j];
+            ossbY1[j] = ycoords[j] + scalar * Math.sin(((Math.PI) / 180) * (curr_unit_val + (neg_pos) * (180) - (neg_pos) * (legs[j - 1].Degree))) * legs[j].Length - scalar * Math.sin(((Math.PI) / 180) * (curr_unit_val + (neg_pos) * (180) - (neg_pos) * (legs[j - 1].Degree))) * OSSB_length[j];
+
+            curr_unit_val = curr_unit_val + (neg_pos) * (180) - (neg_pos) * (legs[j - 1].Degree);
+
+        }
+    }
+
+
+
+
+
+
+
     //Draw the final shape after it has been scaled up!
 
     context.lineJoin = "round";
@@ -407,39 +503,52 @@ if (canvas.getContext) {
         total_crude_length = total_crude_length + legs[k].Length;
     }
 
-
-    if (total_crude_length > 0 && total_crude_length <= 150) {
-        thicc = 4 * scalar;
-    } else if (total_crude_length > 150 && total_crude_length <= 200) {
-        thicc = 5 * scalar;
-    } else if (total_crude_length > 200 && total_crude_length <= 240) {
-        thicc = 6 * scalar;
-    } else {
-        thicc = 1 * scalar;
+    //sets the line width according to the size of the Rebar
+    if (BarSize == 3) {
+        thicc = 15;
+        //thicc = 10;
     }
-
-    //if thicc is too thicc
-    if (thicc > 20) {
-        thicc = max_thicc;
-    }
-    else if (thicc < 17) {
+    else if (BarSize == 4)
+    {
         thicc = 17;
+        //thicc = 12;
     }
-    else {
-        //do nothing
+    else if (BarSize == 5)
+    {
+        thicc = 19;
+        //thicc = 14;
+    }
+    else if (BarSize == 6)
+    {
+        thicc = 21;
+        //thicc = 16;
     }
 
     context.lineWidth = thicc;
 
+    //context.lineWidth = 1;
     //get size of xcoord object to know how many coords are in it
     var num_coords = Object.keys(xcoords).length;
 
     context.beginPath();
     context.moveTo(xcoords[0], ycoords[0]);
-    for (var coord = 1; coord < num_coords; coord++)
+
+    //for (var coord = 1; coord < num_coords; coord++)
+    //{
+    //    context.lineTo(xcoords[coord], ycoords[coord]);
+    //}
+
+    //loop through the OSSB and APEX points and draw the shape with mandrel
+    for (var coord = 0; coord < num_coords - 1; coord++)
     {
-        context.lineTo(xcoords[coord], ycoords[coord]);
+        context.lineTo(ossbX1[coord], ossbY1[coord]);
+        //context.stroke();
+        context.arcTo(apexX[coord], apexY[coord], ossbX2[coord], ossbY2[coord], legs[coord].Mandrel * scalar);
+        //context.stroke();
     }
+    //draw the remaining portion of the last leg of the shape
+    context.lineTo(xcoords[num_coords - 1], ycoords[num_coords - 1]);
+
     context.stroke();
 
 
@@ -451,6 +560,31 @@ if (canvas.getContext) {
     text_y_shift = 0;
     text_2digit_x_shift = 0;
     text_2digit_y_shift = 0;
+
+    //Getting the points of where text is on a shape
+    var label_posX = {};
+    var label_posY = {};
+    for (var leg = 0; leg < num_coords - 1; leg++)
+    {
+        //if first leg
+        if (leg == 0)
+        {
+            label_posX[leg] = (xcoords[leg] + ossbX1[leg]) / 2;
+            label_posY[leg] = (ycoords[leg] + ossbY1[leg]) / 2;
+        }
+        //if last leg
+        else if (leg == num_coords - 2)
+        {
+            label_posX[leg] = (ossbX2[leg - 1] + xcoords[num_coords - 1]) / 2;
+            label_posY[leg] = (ossbY2[leg - 1] + ycoords[num_coords - 1]) / 2;
+        }
+        //if any other leg between first and last
+        else
+        {
+            label_posX[leg] = (ossbX2[leg - 1] + ossbX1[leg]) / 2;
+            label_posY[leg] = (ossbY2[leg - 1] + ossbY1[leg]) / 2;
+        }
+    }
 
     //Drawing the text onto the shape
     context.beginPath();
@@ -465,26 +599,15 @@ if (canvas.getContext) {
         text_2digit_y_shift = 0;
 
         //if unit val is at 0 or 180 or -180 THEN shift straight down
-        if (unit_val_list[coord - 1] == 0 || unit_val_list[coord - 1] == 180 || unit_val_list[coord - 1] == -180)
-        {
+        if (unit_val_list[coord - 1] == 0 || unit_val_list[coord - 1] == 180 || unit_val_list[coord - 1] == -180) {
             if (coord > 9) {
                 text_2digit_x_shift = -5;
             }
             text_y_shift = -4;
         }
 
-            //if unit val is between 0 and -30 or between -180 and -150 THEN diagonal down left
-            else if (0 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -30 || -150 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -180) {
-                if (coord > 9) {
-                    text_2digit_x_shift = -3;
-                }
-                text_x_shift = -3;
-                text_y_shift = -3;
-            }
-
-        //if unit val is at -30 or 150 THEN diagonal down left MORE LEFT THAN DOWN
-        else if (unit_val_list[coord - 1] == -30 || unit_val_list[coord - 1] == 150)
-        {
+        //if unit val is between 0 and -30 or between -180 and -150 THEN diagonal down left
+        else if (0 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -30 || -150 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -180) {
             if (coord > 9) {
                 text_2digit_x_shift = -3;
             }
@@ -492,35 +615,42 @@ if (canvas.getContext) {
             text_y_shift = -3;
         }
 
-            //if unit between -30 and -45 or between 135 and 150 THEN diagonal down left
-            else if (-30 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -45 || 150 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 135) {
-                if (coord > 9) {
-                    text_2digit_x_shift = -3;
-                }
-                text_x_shift = -3;
-                text_y_shift = -3;
+        //if unit val is at -30 or 150 THEN diagonal down left MORE LEFT THAN DOWN
+        else if (unit_val_list[coord - 1] == -30 || unit_val_list[coord - 1] == 150) {
+            if (coord > 9) {
+                text_2digit_x_shift = -3;
             }
+            text_x_shift = -3;
+            text_y_shift = -3;
+        }
+
+        //if unit between -30 and -45 or between 135 and 150 THEN diagonal down left
+        else if (-30 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -45 || 150 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 135) {
+            if (coord > 9) {
+                text_2digit_x_shift = -3;
+            }
+            text_x_shift = -3;
+            text_y_shift = -3;
+        }
 
         //if unit val is at -45 or 135 THEN do nothing
-        else if (unit_val_list[coord - 1] == -45 || unit_val_list[coord - 1] == 135)
-        {
+        else if (unit_val_list[coord - 1] == -45 || unit_val_list[coord - 1] == 135) {
             if (coord > 9) {
                 text_2digit_x_shift = -3;
             }
         }
 
-            //if unit between -45 and -60 or between 135 and 120 THEN diagonal down left
-            else if (-45 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -60 || 135 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 120) {
-                if (coord > 9) {
-                    text_2digit_x_shift = -3;
-                }
-                text_x_shift = -3;
-                text_y_shift = -3;
+        //if unit between -45 and -60 or between 135 and 120 THEN diagonal down left
+        else if (-45 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -60 || 135 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 120) {
+            if (coord > 9) {
+                text_2digit_x_shift = -3;
             }
+            text_x_shift = -3;
+            text_y_shift = -3;
+        }
 
         //if unit val is at -60 or 120 THEN diagonal down left
-        else if (unit_val_list[coord - 1] == -60 || unit_val_list[coord - 1] == 120)
-        {
+        else if (unit_val_list[coord - 1] == -60 || unit_val_list[coord - 1] == 120) {
             if (coord > 9) {
                 text_2digit_x_shift = -3;
             }
@@ -528,13 +658,13 @@ if (canvas.getContext) {
             text_y_shift = -4;
         }
 
-            //if unit is between -60 and -90 or 120 and 90 THEN left
-            else if (-60 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -90 || 120 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 90) {
-                if (coord > 9) {
-                    text_2digit_x_shift = -3;
-                }
-                text_x_shift = -3;
+        //if unit is between -60 and -90 or 120 and 90 THEN left
+        else if (-60 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -90 || 120 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 90) {
+            if (coord > 9) {
+                text_2digit_x_shift = -3;
             }
+            text_x_shift = -3;
+        }
 
         //if unit val is at -90 or 90 THEN move to the left
         else if (unit_val_list[coord - 1] == -90 || unit_val_list[coord - 1] == 90) {
@@ -544,14 +674,14 @@ if (canvas.getContext) {
             text_x_shift = -4;
         }
 
-            //if unit is between -90 and -120 or 90 and 60 THEN diagonal down left MORE LEFT THAN DOWN
-            else if (-90 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -120 || 90 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 60) {
-                if (coord > 9) {
-                    text_2digit_x_shift = -3;
-                }
-                text_x_shift = -4;
-                text_y_shift = -2;
+        //if unit is between -90 and -120 or 90 and 60 THEN diagonal down left MORE LEFT THAN DOWN
+        else if (-90 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -120 || 90 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 60) {
+            if (coord > 9) {
+                text_2digit_x_shift = -3;
             }
+            text_x_shift = -4;
+            text_y_shift = -2;
+        }
 
         //if unit val is at -120 or 60 THEN diagonal down left MORE LEFT THAN DOWN
         else if (unit_val_list[coord - 1] == -120 || unit_val_list[coord - 1] == 60) {
@@ -562,14 +692,14 @@ if (canvas.getContext) {
             text_y_shift = -2;
         }
 
-            //if unit is between -120 and -135 or 60 and 45 THEN diagonal down left
-            else if (-120 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -135 || 60 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 45) {
-                if (coord > 9) {
-                    text_2digit_x_shift = -3;
-                }
-                text_x_shift = -3;
-                text_y_shift = -3;
+        //if unit is between -120 and -135 or 60 and 45 THEN diagonal down left
+        else if (-120 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -135 || 60 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 45) {
+            if (coord > 9) {
+                text_2digit_x_shift = -3;
             }
+            text_x_shift = -3;
+            text_y_shift = -3;
+        }
 
         //if unit val is at -135 or 45 THEN diagonal down left
         else if (unit_val_list[coord - 1] == -135 || unit_val_list[coord - 1] == 45) {
@@ -580,14 +710,14 @@ if (canvas.getContext) {
             text_y_shift = -4;
         }
 
-            //if unit val is between -135 and -150 or 45 and 30 THEN diagonal down left MORE DOWN THAN LEFT
-            else if (-135 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -150 || 45 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 30) {
-                if (coord > 9) {
-                    text_2digit_x_shift = -3;
-                }
-                text_x_shift = -2;
-                text_y_shift = -4;
+        //if unit val is between -135 and -150 or 45 and 30 THEN diagonal down left MORE DOWN THAN LEFT
+        else if (-135 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > -150 || 45 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 30) {
+            if (coord > 9) {
+                text_2digit_x_shift = -3;
             }
+            text_x_shift = -2;
+            text_y_shift = -4;
+        }
 
         //if unit val is at -150 or 30 THEN diagonal down left MORE DOWN THAN LEFT
         else if (unit_val_list[coord - 1] == -150 || unit_val_list[coord - 1] == 30) {
@@ -599,24 +729,27 @@ if (canvas.getContext) {
             text_y_shift = -4;
         }
 
-            //if unit val is between 150 and 180 or -30 and 0 THEN 
-            else if (180 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 150 || 30 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 0) {
-                if (coord > 9) {
-                    text_2digit_x_shift = -3;
-                }
-                text_x_shift = -3;
-                text_y_shift = -5;
+        //if unit val is between 150 and 180 or -30 and 0 THEN 
+        else if (180 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 150 || 30 > unit_val_list[coord - 1] && unit_val_list[coord - 1] > 0) {
+            if (coord > 9) {
+                text_2digit_x_shift = -3;
             }
+            text_x_shift = -3;
+            text_y_shift = -5;
+        }
+        else {
+            //Do nothing!!
+        }
 
-        
+        context.fillText((coord).toString(), text_2digit_x_shift + text_x_shift + label_posX[coord - 1], (-1) * text_2digit_y_shift + (-1) * text_y_shift + label_posY[coord - 1]);
 
-        context.fillText( (coord).toString(), text_2digit_x_shift + text_x_shift + (xcoords[coord - 1] + xcoords[coord]) / 2, (-1) * text_2digit_y_shift + (-1) * text_y_shift + (ycoords[coord - 1] + ycoords[coord]) / 2);
+        //context.fillText( (coord).toString(), text_2digit_x_shift + text_x_shift + (xcoords[coord - 1] + xcoords[coord]) / 2, (-1) * text_2digit_y_shift + (-1) * text_y_shift + (ycoords[coord - 1] + ycoords[coord]) / 2);
     }
     context.stroke();
 
-    console.log("Middle of Big Boi shape currently lies on (" + (max_x + min_x) / 2 + "," + (max_y + min_y) / 2 + ")");
+    console.log("Middle of Big Boi shape on currently lies on (" + (max_x + min_x) / 2 + "," + (max_y + min_y) / 2 + ")");
     console.log("Shape Total Crude Length: " + total_crude_length);
-    console.log("-------------------------------------------------")
+    console.log("-------------------------------------------------------------------------------")
 
     //End of if(canvas.getcontext) block
     }
