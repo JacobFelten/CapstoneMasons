d76f534307bdd12146c486231169e04cd2934980
betettag <betettag@my.lanecc.edu>
2020-06-04 03:11:51 -0700

    emty fields validation , mandrel validation for create cshtml

--- CapstoneMasons/wwwroot/js/createQuote.js
+++ CapstoneMasons/wwwroot/js/createQuote.js
@@ -12,16 +12,153 @@ $('#collapse-payment').on('show.bs.collapse', function () {
 function validateShapes(shape, legs) {
     //var RebarNum = document.getElementById("RebarNum").innerHTML.options;
     
-    //document.getElementById("validationShape" + shape).classList.remove("bg-danger");
+    //document.getElementById("validationShape" + shape).classList.remove("bg-danger");"Shape[@(i)].validation"
     ////document.getElementById(`shape${shape}`).classList.add("bg-warning")
     //document.getElementById("validationShape"+shape).classList.add("bg-success");
     if (legs <= 0 ) {
         document.getElementById("validationShape" + shape).classList.add("bg-danger");
     }
 }
-function vaidateRebar(RebarNumb) {
+
+function validLegCombination(shape) {
+    var barSize = document.querySelector('input[name="'+shape + '.BarSize"]:checked').value;
+    var numbLegs = document.getElementById(`${shape}.LegCount`).value;
+    for (var i = 0; i < numbLegs; i++) {
+        var mandrelItem = `${shape}.leg[${i}].Mandrel.`;
+        switch (barSize) {
+            case "3":
+                document.getElementById(mandrelItem + "None").disabled = false;//every mandrel is available
+                document.getElementById(mandrelItem + "None").checked = true;//preselect lowest mandrel
+                document.getElementById(mandrelItem + "None.Label").classList.add("active");
+                $("#" + mandrelItem + "None.Label").tooltip("hide");
+
+                document.getElementById(mandrelItem + "Small").disabled = false;
+                document.getElementById(mandrelItem + "Small").checked = false;
+                document.getElementById(mandrelItem + "Small.Label").classList.remove("active");
+                $("#" + mandrelItem + "Small.Label").popover("hide");
+
+
+                document.getElementById(mandrelItem + "Medium").disabled = false;
+                document.getElementById(mandrelItem + "Medium").checked = false;
+                document.getElementById(mandrelItem + "Medium.Label").classList.remove("active");
+                $("#" + mandrelItem + "Medium.Label").popover("hide");
+
+                document.getElementById(mandrelItem + "Large").checked = false;
+                document.getElementById(mandrelItem + "Large.Label").classList.remove("active");
+                break;
+            case "4":
+                document.getElementById(mandrelItem + "None").disabled = true;//small and med and large available
+                document.getElementById(mandrelItem + "None").checked = false;//small preselected
+                document.getElementById(mandrelItem + "None.Label").classList.remove("active");
+                $("#" + mandrelItem + "None.Label").tooltip({ title: "Invalid Bar Size for this Mandrel", trigger: "hover", placement: "bottom" });
+                $("#" + mandrelItem + "None.Label").tooltip();
+
+                document.getElementById(mandrelItem + "Small").disabled = false;
+                document.getElementById(mandrelItem + "Small").checked = true;
+                document.getElementById(mandrelItem + "Small.Label").classList.add("active");
+                $("#" + mandrelItem + "Small.Label").popover("hide");
+
+                document.getElementById(mandrelItem + "Medium").disabled = false;
+                document.getElementById(mandrelItem + "Medium").checked = false;
+                document.getElementById(mandrelItem + "Medium.Label").classList.remove("active");
+                $("#" + mandrelItem + "Medium.Label").tooltip("hide");
+
+                document.getElementById(mandrelItem + "Large").checked = false;
+                document.getElementById(mandrelItem + "Large.Label").classList.remove("active");
+                break;
+            case "5":
+                document.getElementById(mandrelItem + "None").disabled = true;
+                document.getElementById(mandrelItem + "None").checked = false;
+                document.getElementById(mandrelItem + "None.Label").classList.remove("active");
+                $("#" + mandrelItem + "None.Label").popover({ content: "Invalid Bar Size for this Mandrel", trigger: "hover", placement: "bottom" });
+
+                document.getElementById(mandrelItem + "Small").disabled = true;
+                document.getElementById(mandrelItem + "Small").checked = false;
+                document.getElementById(mandrelItem + "Small.Label").classList.remove("active");
+                $("#" + mandrelItem + "Small.Label").popover({ content: "Invalid Bar Size for this Mandrel", trigger: "hover", placement: "bottom" });
+
+                document.getElementById(mandrelItem + "Medium").disabled = false;//med and large available
+                document.getElementById(mandrelItem + "Medium").checked = true;//medium preselected
+                document.getElementById(mandrelItem + "Medium.Label").classList.add("active");
+                $("#" + mandrelItem + "Medium.Label").popover("hide");
+
+                document.getElementById(mandrelItem + "Large").checked = false;
+                document.getElementById(mandrelItem + "Large.Label").classList.remove("active");
+                break;
+            case "6":
+                document.getElementById(mandrelItem + "None").disabled = true;
+                document.getElementById(mandrelItem + "None").checked = false;
+                document.getElementById(mandrelItem + "None.Label").classList.remove("active");
+                $("#" + mandrelItem + "None.Label").popover({ content: "Invalid Bar Size for this Mandrel", trigger: "hover", placement: "bottom" });
+
+                document.getElementById(mandrelItem + "Small").disabled = true;
+                document.getElementById(mandrelItem + "Small").checked = false;
+                document.getElementById(mandrelItem + "Small.Label").classList.remove("active");
+                $("#" + mandrelItem + "Small.Label").popover({ content: "Invalid Bar Size for this Mandrel", trigger: "hover", placement: "bottom" });
+
+                document.getElementById(mandrelItem + "Medium").disabled = true;
+                document.getElementById(mandrelItem + "Medium").checked = false;
+                document.getElementById(mandrelItem + "Medium.Label").classList.remove("active");
+                $("#" + mandrelItem + "Medium.Label").popover({ content: "Invalid Bar Size for this Mandrel", trigger: "hover", placement: "bottom" });
+
+                document.getElementById(mandrelItem + "Large").checked = true;//large available and preselected
+                document.getElementById(mandrelItem + "Large.Label").classList.add("active");
+                break;
+        }
+    }
+}
+
+function checkFormFields(shape) {
+    classList = document.getElementById(shape + '.Validation').classList;
+    if (classList.contains('bg-warning')) {
+        document.getElementById(shape + '.Validation').classList.remove("bg-warning");
+    } else if (classList.contains('bg-success')) {
+        document.getElementById(shape + '.Validation').classList.remove("bg-success");
+    }
+    document.getElementById(shape + '.Outline').style.outline = "";
+    var fields = getAllFormValues(shape);
+    var i, l = fields.length;
+    var fieldname;
+    var result = ""
+    for (i = 0; i < l && result.length == 0; i++) {
+        fieldname = shape+fields[i];
+        if (document.forms['CreateQuote'][fieldname].value === "" ||
+            document.forms['CreateQuote'][fieldname].value === "0") {
+            //show invalid shape when empty
+            result = fields[i] + " can not be empty";
+            
+
+        } else if (document.forms['CreateQuote'][fieldname].value <= -1) {
+            //show invalid shape when negative
+            result = fields[i] + " can not be negative";
+        }
+    }
+    if (result.length > 0) {
+        document.getElementById(shape + '.Validation').classList.add("bg-warning");
+        document.getElementById(shape + '.Validation').innerHTML = result; //styling changes for a invalid shape
+        document.getElementById(shape + '.Outline').style.outline = "6px solid #ffc107";
+        document.getElementById('CreateQuote.Submit').disabled = true;
+    } else {
+        document.getElementById(shape + '.Validation').classList.add("bg-success");
+        document.getElementById(shape + '.Validation').innerHTML = "Valid Shape";//styling changes for a valid shape
+        document.getElementById(shape + '.Outline').style.outline = "6px solid #28a745";
+        document.getElementById('CreateQuote.Submit').disabled = false;
+    }
 
 }
+
+function getAllFormValues(shape) {
+    var fields = [".Qty", ".BarSize", ".Legs[0].Length"];//will add more fields as the shape increases
+    var numbLegs = document.getElementById(shape + '.LegCount').value;
+    if (numbLegs > 0) {
+        for (var legIndex = 1; legIndex <= numbLegs; ++legIndex) {
+            fields.push(`.Legs[${(legIndex - 1)}].Degree`, `.Legs[${(legIndex - 1)}].IsRight`, `.Legs[${(legIndex - 1)}].Mandrel.Name`, `.Legs[${legIndex}].Length`);
+        }
+    }
+
+    return fields;
+}
+
 function deleteShape(ShapeNumb) {
     var numbShapes = document.getElementById(`Shapes.Count`);
     
